c79c033a3714a2a435ce7adbc699f8de9e7d6411
Antti Laiti <antti@laiti.fi>
2024-10-26 19:00:23 +0300

    Add comment about provider

--- server.tf
+++ server.tf
@@ -54,6 +54,10 @@ resource "upcloud_server" "server" {
   }
 }
 
+
+# UpCloud Terraform provider does not support setting PTR record for a host
+# but their API does so we set it via this script.
+
 resource "null_resource" "set_ptr_record" {
   provisioner "local-exec" {
     command = "tools/set-ptr-record.sh ${local.server_public_ipv4_address} ${local.server_hostname}"
