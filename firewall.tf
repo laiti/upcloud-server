25622aaa8cbf1bda4b75c5e19119e400bb0ad47b
Antti Laiti <antti@laiti.fi>
2024-10-26 20:15:07 +0300

    Change firewall rule order

--- firewall.tf
+++ firewall.tf
@@ -1,22 +1,6 @@
 resource "upcloud_firewall_rules" "server_firewall" {
     server_id = upcloud_server.server.id
 
-    // Add SSH allow rules for each network in ssh_allow list
-    dynamic "firewall_rule" {
-        for_each = var.firewall_allow
-        content {
-            action                 = "accept"
-            direction              = "in"
-            comment                = "Allow ${firewall_rule.value.name}"
-            destination_port_start = firewall_rule.value.port_start
-            destination_port_end   = try(firewall_rule.value.port_end, firewall_rule.value.port_start)
-            family                 = try(firewall_rule.value.family, null)
-            protocol               = try(firewall_rule.value.protocol, null)
-            source_address_start   = try(firewall_rule.value.start, null)
-            source_address_end     = try(firewall_rule.value.end, firewall_rule.value.start, null)
-        }
-    }
-
     // Create firewall rules for DNS entries in upcloud_dns. TF does not support nested for_each statements so
     // we create TCP and UDP rules separately
     dynamic "firewall_rule" {
@@ -48,6 +32,22 @@ resource "upcloud_firewall_rules" "server_firewall" {
             protocol               = "tcp"
             source_address_start   = firewall_rule.value
             source_address_end     = firewall_rule.value
+        }
+    }
+
+    // Add SSH allow rules for each network in ssh_allow list
+    dynamic "firewall_rule" {
+        for_each = var.firewall_allow
+        content {
+            action                 = "accept"
+            direction              = "in"
+            comment                = "Allow ${firewall_rule.value.name}"
+            destination_port_start = firewall_rule.value.port_start
+            destination_port_end   = try(firewall_rule.value.port_end, firewall_rule.value.port_start)
+            family                 = try(firewall_rule.value.family, null)
+            protocol               = try(firewall_rule.value.protocol, null)
+            source_address_start   = try(firewall_rule.value.start, null)
+            source_address_end     = try(firewall_rule.value.end, firewall_rule.value.start, null)
         }
     }
 
