1802d9350d295b3830437be1e6048fe3fcda76eb
Antti Laiti <antti@laiti.fi>
2024-10-26 19:24:44 +0300

    Add IPv6 support for PTR record

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
-    server_hostname            = upcloud_server.server.hostname
-    server_public_ipv4_address = upcloud_server.server.network_interface[0].ip_address
+    server_hostname             = upcloud_server.server.hostname
+    server_public_ipv4_address  = upcloud_server.server.network_interface[0].ip_address
+    server_public_ipv6_address  = upcloud_server.server.network_interface[1].ip_address
 }
